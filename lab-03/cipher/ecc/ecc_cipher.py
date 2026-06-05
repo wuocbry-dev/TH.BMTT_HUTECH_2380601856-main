161b67b6bc186dec3543178225f9ae9e64fcd460
QuocBry-Dev <nguyenwuoc2004.dev@gmail.com>
2026-06-05 21:15:22 +0700

    Add lab 03 public key cipher app

---
+++ lab-03/cipher/ecc/ecc_cipher.py
@@ -0,0 +1,38 @@
+import ecdsa, os
+
+if not os.path.exists('cipher/ecc/keys'):
+    os.makedirs('cipher/ecc/keys')
+
+class ECCCipher:
+    def __init__(self):
+        pass
+
+    def generate_keys(self):
+        sk = ecdsa.SigningKey.generate()  # Tạo khóa riêng tư
+        vk = sk.get_verifying_key()  # Lấy khóa công khai từ khóa riêng tư
+
+        with open('cipher/ecc/keys/privateKey.pem', 'wb') as p:
+            p.write(sk.to_pem())
+
+        with open('cipher/ecc/keys/publicKey.pem', 'wb') as p:
+            p.write(vk.to_pem())
+
+    def load_keys(self):
+        with open('cipher/ecc/keys/privateKey.pem', 'rb') as p:
+            sk = ecdsa.SigningKey.from_pem(p.read())  # Đúng kiểu dữ liệu
+
+        with open('cipher/ecc/keys/publicKey.pem', 'rb') as p:
+            vk = ecdsa.VerifyingKey.from_pem(p.read())
+
+        return {'private_key': sk, 'public_key': vk}  # Đảm bảo sk là SigningKey
+
+
+    def sign(self, message, key):
+        # Ký dữ liệu bằng khóa riêng tư
+        return key.sign(message.encode('ascii'))
+
+    def verify(self, message, signature, key):
+        try:
+            return key.verify(signature, message.encode('ascii'))
+        except ecdsa.BadSignatureError:
+            return False
